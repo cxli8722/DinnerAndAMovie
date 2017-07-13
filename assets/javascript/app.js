b74c3c43383ef50620dd3444cbacce5437872a46
brostami <brostami@users.noreply.github.com>
2017-07-13 06:53:28 -0400

    got radius

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
-
 $("#welcome-modal").modal("show");
 $("#lets-go").on("click", function(event) {
 
   $("#welcome-modal").modal("hide");
   $("#initial-form").modal("show");
-     
 });   
 
 var map;
@@ -12,7 +10,7 @@ var infowindow;
 var lat = 0;
 var lng = 0;
 var keyword = "park";
-
+var radiusValue = 50000;
 
 //ajax for movie data for specific chosen location 
 function movieDisplay (theaterLat, theaterLng){
@@ -73,6 +71,7 @@ function createMarker(place) {
   });
 }
 
+// Place markers on map
 function placeMarkers(results, status) {            
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
@@ -82,6 +81,7 @@ function placeMarkers(results, status) {
   }
 }
 
+// Display map with search results
 function initMap(lat, lng, keyword) { // use lat and lng 
   var latLngObj = {lat: lat, lng: lng};
   console.log("lat1: " + lat);
@@ -96,67 +96,59 @@ function initMap(lat, lng, keyword) { // use lat and lng
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch({
     location: latLngObj ,
-    radius: 50000,
+    radius: radiusValue,
     keyword: [keyword]
   }, placeMarkers);
 }
 
+// Get lat and lng to pass to movie API
 function getlocation(address, keyword){
   console.log("getlocation: " + address + keyword);
-   axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
-        params:{
-          address:address,
-          key:'AIzaSyBO59mo6rMe4ChzmBqEQ8gz9QmWjg_X38c'
-        }
-      })
-      .then(function(response){
-        // Log full response
-        console.log(response);
-
-        var addressComponents = response.data.results[0].address_components;
-        var addressComponentsOutput = '<ul class="list-group">';
-       
-        lat = response.data.results[0].geometry.location.lat;
-        lng = response.data.results[0].geometry.location.lng;
-
-        console.log("latt: " + lat)
-
-        console.log("lngg: " + lng)
-        initMap(lat, lng, keyword);
-       
-      })
-      .catch(function(error){
-      console.log(error);
-      });
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+    params:{
+      address:address,
+      key:'AIzaSyBO59mo6rMe4ChzmBqEQ8gz9QmWjg_X38c'
     }
+  }).then(function(response){
+    // Log full response
+    console.log(response);
 
+    var addressComponents = response.data.results[0].address_components;
+    var addressComponentsOutput = '<ul class="list-group">';
+    
+    lat = response.data.results[0].geometry.location.lat;
+    lng = response.data.results[0].geometry.location.lng;
+
+    console.log("latt: " + lat)
+
+    console.log("lngg: " + lng)
+    initMap(lat, lng, keyword);
+    
+  }).catch(function(error){
+    console.log(error);
+  });
+}
+
+// Click event for initial form submit
 $("#sumbitbtn").on("click", function(event) {
-  // Don't refresh the page!
-
   event.preventDefault();
   $("#initial-form").modal("hide");
-
   var zipCode = $("#zipCodeInput").val().trim();
-  //var zipCode = document.getElementById('zipCodeInput').value;
-  console.log("zipcode:" + zipCode);
-  // call the fucntion getlocation()
-    
+  console.log("zipcode:" + zipCode);    
   keyword = "movie_theater";
   console.log("keyword set: " + keyword);
-    getlocation(zipCode, keyword);
-    // $("#food-form").modal("show");
+  getlocation(zipCode, keyword);
+  // $("#food-form").modal("show");
 });
 
+// Click event for food form submit
 $("#food-sumbit").on("click", function(event) {
-  // Don't refresh the page!
   event.preventDefault();
   $("#food-form").modal("hide");
   foodInput();
-  //var foodtype = $('input[type=checkbox]:checked').val().trim();
-  //var zipCode = document.getElementById('zipCodeInput').value;
 });
 
-   
+// Get values of checked food choices and put markers on the map   
 function foodInput() {
   var checkedValue = null; 
   var inputElements = document.getElementsByClassName('messageCheckbox');
@@ -164,11 +156,16 @@ function foodInput() {
     if(inputElements[i].checked){
       checkedValue = inputElements[i].value;
       console.log(checkedValue );
-      //var address="8030 Renaissance Pkwy, Durham, NC 27713";
-      //getlocation(address, checkedValue);
       initMap(lat, lng, checkedValue);
     }
   }
 }
 
-// $("#initial-form").modal("show");
+// Get Radius
+$(".dropdown-item").on("click", function() {
+  var radiusMiles = parseInt($(this).attr('value'));
+  radiusValue =  (radiusMiles * 1609.344);
+  var buttonText = $(this).text();
+  console.log("Radius: " + radiusMiles + " miles, " + radiusValue + " meters");
+  $("#radius").text(buttonText);
+});
